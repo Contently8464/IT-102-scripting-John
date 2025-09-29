c6258cb89abf3f5eeed5bdb9e2a9c5c4fa6fa998
contently8464 <gurney.john@student.greenriver.edu>
2025-09-29 12:31:37 -0700

    update chapter 2 activity

--- start/CH02/HelloWorld.py
+++ start/CH02/HelloWorld.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
 # A simple "Hello World" script in python
-# Created 
+# Created by John
+name = input("What is your name? ")
+positive_affirmation = ("I am joyed to learn Python! ")
+age = int(input("In whole numbers, what is your age in years? "))
+
+print(name)
+print("In 2 years you will be this old: ")
+print(age + 2)
+print(positive_affirmation)
