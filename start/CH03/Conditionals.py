6d0eb1c323da13c465fb2bc9456795ab0fe5bc30
contently8464 <gurney.john@student.greenriver.edu>
2025-10-21 08:03:46 -0700

    updating conditionals chapter 3

--- start/CH03/Conditionals.py
+++ start/CH03/Conditionals.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python3
 # example workign with conditionals
 #By 
+vibe = input('Is today a good day? (y/n) ')
+if vibe == '':
+    print ("Please input y or n")
+elif vibe == "y":
+    print ("I'm glad you're having a good day")
+else:
+    print ("I'm sorry to hear that.")
