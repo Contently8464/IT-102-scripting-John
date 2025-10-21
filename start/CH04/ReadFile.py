0462d1b36a4a19b43489d87702499b2c905f12c2
contently8464 <gurney.john@student.greenriver.edu>
2025-10-21 08:43:19 -0700

    updated read files chapter 4

--- start/CH04/ReadFile.py
+++ start/CH04/ReadFile.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python3
 # Sample script that reads from a file
-# By 
+# By John
+with open("hackme.txt", "r") as example:
+    content = example.read()
+    print("Here is someone to hack - information: ")
+    print(content)
