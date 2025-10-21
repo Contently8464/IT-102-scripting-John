3e70cb221a8ccfebb98b2304f7dc2b41f6e51f5d
contently8464 <gurney.john@student.greenriver.edu>
2025-10-21 08:07:09 -0700

    updating loopy chapter 3

--- start/CH03/Loopy.py
+++ start/CH03/Loopy.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
 # example workign with Loops
 #By 
+vibe = input('Is today a good day? (y/n) ')
+if vibe == 'y':
+    number = 1
+    while number < 11:
+        print ("I'm glad you're having a good day")
+        number += 1
+elif vibe == "n":
+    print ("I'm sorry to hear that.")
+else:
+    print ("Please input y or n")
