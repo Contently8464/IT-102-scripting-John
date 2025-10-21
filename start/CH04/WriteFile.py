7332332299ba1267815bff0c8886523c706e9186
contently8464 <gurney.john@student.greenriver.edu>
2025-10-21 08:41:11 -0700

    updated  writefile chapter 4

--- start/CH04/WriteFile.py
+++ start/CH04/WriteFile.py
@@ -1,3 +1,24 @@
 #!/usr/bin/env python3
 # Sample script that writes to a file
-# By 
+# By John  
+questions = [
+    "What is your name? ",
+    "What is your favorite color? ",
+    "What was your first pet's name? ",
+    "What is yourmother's maiden name? ",
+    "What elementary school did you attend? ",
+
+]
+
+answers = []
+for q in questions:
+    ans = input(q)
+    answers.append(ans)
+
+with open("hackme.txt", "w") as file:
+    for line in answers:
+        file.write(line + "\n")
+
+with open("hackme.txt", "r") as example:
+    content = example.read()
+    print(content)
