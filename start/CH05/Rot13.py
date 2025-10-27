6fd83b211df3d1c81b212867e0d1b0d844f0466d
contently8464 <gurney.john@student.greenriver.edu>
2025-10-27 11:09:16 -0700

    update rot13

--- start/CH05/Rot13.py
+++ start/CH05/Rot13.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python3
 # Script that encrypts/decrypts text using ROT13
-# By 
+# By John 
+
+def rot13(text,shift):
+    result = ""
+    for char in text:
+        if char.isalpha():
+            base = ord('A') if char.isupper() else ord('a')
+            result += chr((ord(char) - base + shift) % 26 + base)
+        else:
+            result += char
+        return result
+message = input("Please input your text to rotate, no spaces or numbers: ")
+shift = 13
+encrypted = rot13(message, shift)
+decrypted = rot13(encrypted, -shift)
+print(f"encrypted {encrypted}")
+print(f"encrypted {decrypted}")
